98235d2f9416b5a73c089a2fea5ee547d41a0e5d
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-06-21 02:52:38 +0530

    Bux fix in comment model

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -5,7 +5,7 @@ const { DateTime } = require("luxon");
 
 const commentSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  post: { type: Schema.Types.ObjectId, ref: "Comment", required: true },
+  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
   text: { type: String, required: true },
 });
 
