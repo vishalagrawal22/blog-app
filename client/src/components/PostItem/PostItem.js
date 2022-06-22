df456a4583960ec5f008b002cece843d10c06cd3
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-06-23 03:55:30 +0530

    Add author page link in post

--- client/src/components/PostItem/PostItem.js
+++ client/src/components/PostItem/PostItem.js
@@ -3,7 +3,11 @@ export default function PostItem({ post }) {
     <div className="flex-grow-1 p-3 d-flex flex-column align-items-center">
       <h2 className="mb-3">{post.title}</h2>
       <h5 className="text-secondary mb-5">
-        By {post.author.name} (Posted on {post.createdAt})
+        By{" "}
+        <a href={post.author.url} className="text-black">
+          {post.author.name}
+        </a>{" "}
+        (Posted on {post.createdAt})
       </h5>
       <div style={{ whiteSpace: "pre-wrap" }}>{post.description}</div>
     </div>
