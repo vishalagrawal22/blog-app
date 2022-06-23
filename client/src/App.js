4a28e1fc2fbd738e566893ae1ebf94a35715d3b6
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-06-23 12:49:35 +0530

    Make add post button only appear on feed page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import UserContext from "./UserContext";
 import { useCurrentUser } from "./hooks/auth";
 import Header from "./components/Header";
-import addPostImageSrc from "./images/add.svg";
 
 function App() {
-  const { user, isLoading, addAccessToken, clearAccessToken } = useCurrentUser();
-  const navigate = useNavigate();
+  const { user, isLoading, addAccessToken, clearAccessToken } =
+    useCurrentUser();
   return (
     <>
       <UserContext.Provider
@@ -15,24 +14,6 @@ function App() {
         <Header />
         <Outlet />
       </UserContext.Provider>
-      {user && (
-        <img
-          title="add post"
-          style={{
-            width: "60px",
-            height: "60px",
-            position: "fixed",
-            right: 30,
-            zIndex: 1,
-            bottom: 25,
-          }}
-          src={addPostImageSrc}
-          onClick={() => {
-            navigate("/posts/create");
-          }}
-          alt="add post"
-        />
-      )}
     </>
   );
 }
--- client/src/page/Feed/Feed.js
+++ client/src/page/Feed/Feed.js
@@ -1,7 +1,15 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import UserContext from "../../UserContext";
+
 import { usePosts } from "../../hooks/posts";
 import PostCardList from "../../components/PostCardList";
+import addPostImageSrc from "../../images/add.svg";
 
 export default function Feed() {
+  const navigate = useNavigate();
+  const { user } = useContext(UserContext);
   const { posts, isLoading, error } = usePosts(
     `${process.env.REACT_APP_API_URL}/posts`
   );
@@ -11,6 +19,28 @@ export default function Feed() {
   } else if (error) {
     return <div className="m-3">{error.message || error.msg}</div>;
   } else {
-    return <PostCardList posts={posts} />;
+    return (
+      <>
+        <PostCardList posts={posts} />
+        {user && (
+          <img
+            title="add post"
+            style={{
+              width: "60px",
+              height: "60px",
+              position: "fixed",
+              right: 30,
+              zIndex: 1,
+              bottom: 25,
+            }}
+            src={addPostImageSrc}
+            onClick={() => {
+              navigate("/posts/create");
+            }}
+            alt="add post"
+          />
+        )}
+      </>
+    );
   }
 }
